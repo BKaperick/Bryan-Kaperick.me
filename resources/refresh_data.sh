6795dec846d5426da898fceeca6c6f6c7171b29d
Bryan Kaperick <bryan.kaperick@gmail.com>
2023-10-09 22:24:34 +0100

    Git pull before pushing

--- resources/refresh_data.sh
+++ resources/refresh_data.sh
@@ -3,6 +3,7 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 python $DIR/datagetter.py $DIR/../content/datapoint.txt
 python $DIR/write_scraped_data.py $DIR/../content/argot.txt
 echo "$DIR"
+git -C /home/bryan/Bryan-Kaperick.me pull origin --ff-only
 git -C /home/bryan/Bryan-Kaperick.me add "$DIR/../content/datapoint.txt"
 git -C /home/bryan/Bryan-Kaperick.me add "$DIR/../content/argot.txt"
 git -C /home/bryan/Bryan-Kaperick.me commit -m "Update data point"
