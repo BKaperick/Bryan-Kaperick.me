0d12fbde1132c6dcf4a01e1046a7dcb5cf1210af
Bryan Kaperick <bryankaperick@gmail.com>
2024-08-16 23:14:37 +0200

    fix footer translation

--- content/footer.php
+++ content/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer layout__footer">
-<p><span>© Bryan Kaperick&#39;s Website </span> <span style="color:grey"> -- Site last updated  <?php include($_SERVER['DOCUMENT_ROOT']."/content/widgets/last_update_date.txt"); ?>
+<p><span>© <?php echo $language['SITE_TITLE'] ?></span> <span style="color:grey"> -- <?php echo $language['Site last updated'] ?>  <?php include($_SERVER['DOCUMENT_ROOT']."/content/widgets/last_update_date.txt"); ?>
 </span></p>
 
 
