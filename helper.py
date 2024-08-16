8d1b9f4636d0e3d9f441f384fc97512c78ce61e0
Bryan Kaperick <bryankaperick@gmail.com>
2024-08-17 00:47:05 +0200

    can't have integer in key

--- helper.py
+++ helper.py
@@ -13,7 +13,7 @@ months = {
 'Dec': 11
 }
 
-invalid_key_chars = [r"<", r">", ",", ".", r"&rsquo;", "'", "-"]
+invalid_key_chars = [r"<", r">", ",", ".", r"&rsquo;", "'", "-", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 def clean_key(w):
     for c in invalid_key_chars:
         w = w.replace(c,"")
