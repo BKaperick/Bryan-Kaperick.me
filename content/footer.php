8438dfcae3fd2e5f99536a2604681ee2f7baa49b
Bryan Kaperick <bryankaperick@gmail.com>
2024-08-16 23:10:14 +0200

    use date in footer.php

--- content/footer.php
+++ content/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer layout__footer">
-<p><span>© Bryan Kaperick&#39;s Website </span> <span style="color:grey"> -- Site last updated  2024-08-13 17:54:57
+<p><span>© Bryan Kaperick&#39;s Website </span> <span style="color:grey"> -- Site last updated  <?php include($_SERVER['DOCUMENT_ROOT']."/content/widgets/last_update_date.txt"); ?>
 </span></p>
 
 
