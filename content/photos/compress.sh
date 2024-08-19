0aeeeafb3263148b0492274cf370e5c971ac330b
Bryan Kaperick <bryankaperick@gmail.com>
2024-08-19 11:12:17 +0200

    make compress work with gh actions ubuntu

--- content/photos/compress.sh
+++ content/photos/compress.sh
@@ -3,7 +3,11 @@ for file in ./*
 do
     echo $file
     # magick -strip -interlace Plane -gaussian-blur 0.05 -quality 85% file 
-    magick $file -strip -interlace Plane -gaussian-blur 0.05 -quality 85% "../lowres/$file"
+
+    # TODO when ubuntu supports magick (updated version of convert) we can use this commented
+    # line, but for now gh actions is on an ubuntu container
+    #magick $file -strip -interlace Plane -gaussian-blur 0.05 -quality 85% "../lowres/$file"
+    convert -strip -interlace Plane -gaussian-blur 0.05 -quality 85% $file  "../lowres/$file"
 done
 cd ..
 
