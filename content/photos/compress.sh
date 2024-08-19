fd278f5b42ff28e3614c694800bfa04985265f13
Bryan Kaperick <bryankaperick@gmail.com>
2024-08-19 12:33:10 +0200

    compress bigger files more

--- content/photos/compress.sh
+++ content/photos/compress.sh
@@ -1,13 +1,18 @@
 cd raw
 for file in ./*
 do
-    echo $file
-    # magick -strip -interlace Plane -gaussian-blur 0.05 -quality 85% file 
-
     # TODO when ubuntu supports magick (updated version of convert) we can use this commented
     # line, but for now gh actions is on an ubuntu container
     #magick $file -strip -interlace Plane -gaussian-blur 0.05 -quality 85% "../lowres/$file"
     convert -strip -interlace Plane -gaussian-blur 0.05 -quality 75% $file  "../lowres/$file"
+done
+for file in $( find . -size +1M )
+do
+    echo "Large file: $file"
+    # TODO when ubuntu supports magick (updated version of convert) we can use this commented
+    # line, but for now gh actions is on an ubuntu container
+    #magick $file -strip -interlace Plane -gaussian-blur 0.05 -quality 85% "../lowres/$file"
+    convert -strip -interlace Plane -gaussian-blur 0.05 -quality 25% $file  "../lowres/$file"
 done
 cd ..
 
