80dc5e30ca8eab9c681636a7dc4877b4a17b8f0f
Bryan Kaperick <bryan.kaperick@gmail.com>
2024-08-20 10:39:48 +0100

    argot changes

--- resources/refresh_data.sh
+++ resources/refresh_data.sh
@@ -5,6 +5,7 @@ python $DIR/write_scraped_data.py $DIR/../content/widgets/argot.txt
 echo "$DIR"
 git -C /home/bryan/Bryan-Kaperick.me pull origin --ff-only
 git -C /home/bryan/Bryan-Kaperick.me add "$DIR/../content/widgets/datapoint.txt"
+git -C /home/bryan/Bryan-Kaperick.me add "www*"
 git -C /home/bryan/Bryan-Kaperick.me add "$DIR/../content/widgets/argot.txt"
 git -C /home/bryan/Bryan-Kaperick.me commit -m "Update data point"
 git -C /home/bryan/Bryan-Kaperick.me push
