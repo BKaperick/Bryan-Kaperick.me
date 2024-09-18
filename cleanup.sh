c97c561fc372b4845f9d2e6cb9eabc6a52a2faa3
Bryan Kaperick <bryankaperick@gmail.com>
2024-09-18 11:41:48 +0200

    more fix cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -22,13 +22,22 @@ find . -type f -name '*.orig' -delete
 find . -type f -wholename "./content/*" -exec sed -i -E "s/href=\"(.*).php%3Flang=(\w\w)/href=\"\/\2\/\1\/\1.php/g" {} \;
 #find . -type f -wholename "./content/*" -exec sed -i -E "s/href=\"(.*).php%3Flang=(\w\w)/href=\".\/\1.php/g" {} \;
 
-find . -type f -name "index_fr.html" -exec sed -i -E "s/index.html%3Flang=en.html/index.html/g" {} \;
-find . -type f -name "index.html" -exec sed -i -E "s/index.html%3Flang=fr.html/index_fr.html/g" {} \;
+find . -type f -name "index_fr.html" -exec sed -i -E "s/index\.html%3Flang=en\.html/index.html/g" {} \;
+find . -type f -name "index.html" -exec sed -i -E "s/index\.html%3Flang=fr\.html/index_fr.html/g" {} \;
 
 # All links on french pages back to index.html need to point to index_fr.html instead.
-find . -type f -wholename "./content/fr/*" -exec sed -i -E "s/index.html/index_fr.html/g" {} \;
+find . -type f -wholename "./content/fr/*" -exec sed -i -E "s/index\.html/index_fr.html/g" {} \;
 # All links to `index.html` in `index_fr.html` except the language toggle need to point to `index_fr.html`
-find . -type f -name "index_fr.html" -exec sed -i -E 's/<a href="index.html" title="Home"/<a href="index_fr.html" title="Home"/g' {} \;
+find . -type f -name "index_fr.html" -exec sed -i -E 's/<a href="index\.html" title="Home"/<a href="index_fr.html" title="Home"/g' {} \;
+
+# At the end, rename everything to use `.html` rather than the uglier `.php.html`. 
+for f in $(find . -maxdepth 10 -type f -name '*php.html'); do
+    newname=`echo "$f" | sed 's/.php.html/.html/'`
+    echo "current file is $f -> $newname"
+    mv "$f" "$newname"
+done
+find . -type f -wholename "*" -exec sed -i -E "s/\.php\.html/.html/g" {} \;
+
 
 # HACK TODO fix blog post urls, `header.php` creates language toggle but doesnt have access to current path
 find . -type f -wholename "./content/*/blog-post/*" -exec sed -i -E "s/\/(\w\w)\/(.*)\/(.*)\.html/\/\1\/blog-post\/\3.html/g" {} \;
