0d98cd987127f35ae63de5fb2727dd882833dd85
Bryan Kaperick <bryankaperick@gmail.com>
2024-09-19 16:35:20 +0200

    Skip already-compressed files

--- compress.sh
+++ compress.sh
@@ -1,18 +1,22 @@
 cd raw
 for file in ./*
 do
-    # TODO when ubuntu supports magick (updated version of convert) we can use this commented
-    # line, but for now gh actions is on an ubuntu container
-    #magick $file -strip -interlace Plane -gaussian-blur 0.05 -quality 85% "../lowres/$file"
-    convert -strip -interlace Plane -gaussian-blur 0.05 -quality 75% $file  "../lowres/$file"
+    if ! test -f "../lowres/$file"; then
+        # TODO when ubuntu supports magick (updated version of convert) we can use this commented
+        # line, but for now gh actions is on an ubuntu container
+        #magick $file -strip -interlace Plane -gaussian-blur 0.05 -quality 85% "../lowres/$file"
+        convert -strip -interlace Plane -gaussian-blur 0.05 -quality 75% $file  "../lowres/$file"
+    fi
 done
 for file in $( find . -size +1M )
 do
-    echo "Large file: $file"
-    # TODO when ubuntu supports magick (updated version of convert) we can use this commented
-    # line, but for now gh actions is on an ubuntu container
-    #magick $file -strip -interlace Plane -gaussian-blur 0.05 -quality 85% "../lowres/$file"
-    convert -strip -interlace Plane -gaussian-blur 0.05 -quality 25% $file  "../lowres/$file"
+    if ! test -f "../lowres/$file"; then
+        echo "Large file: $file"
+        # TODO when ubuntu supports magick (updated version of convert) we can use this commented
+        # line, but for now gh actions is on an ubuntu container
+        #magick $file -strip -interlace Plane -gaussian-blur 0.05 -quality 85% "../lowres/$file"
+        convert -strip -interlace Plane -gaussian-blur 0.05 -quality 25% $file  "../lowres/$file"
+    fi
 done
 cd ..
 
