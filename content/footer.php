7a5292b8549752ff0c6046c3949589d43136ba1f
Bryan Kaperick <bryankaperick@gmail.com>
2024-10-02 15:05:29 +0200

    footer tweaks: dont break up date across lines, better color

--- content/footer.php
+++ content/footer.php
@@ -1,7 +1,9 @@
 <footer class="footer layout__footer">
-<p><span>© <?php echo $language['SITE_TITLE'] ?></span> <span style="color:grey"> – <?php echo $language['Site last updated'] ?>  <?php include($_SERVER['DOCUMENT_ROOT']."/widgets/last_update_date.txt"); ?></span>
-<br><span style="color:grey"><?php echo $language['SKETCH_CRED'] ?> </span><a href="https://www.instagram.com/j.andrews96/">Jason Andrews</a>
+<span style="color:grey"> 
+<p><span>© <?php echo $language['SITE_TITLE'] ?></span> – <nobr><?php echo $language['Site last updated'] ?>  <?php include($_SERVER['DOCUMENT_ROOT']."/widgets/last_update_date.txt"); ?></nobr>
+<br>
+<?php echo $language['SKETCH_CRED'] ?> <a href="https://www.instagram.com/j.andrews96/" style="color:#585858">Jason Andrews</a>
 </p>
-
+</span>
 
 </footer>
