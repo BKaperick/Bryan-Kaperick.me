e8c38d85095c7a55fff097cea996bffc169277d2
Bryan Kaperick <bryankaperick@gmail.com>
2024-10-10 17:00:13 +0200

    add projects php

---
+++ content/projects.php
@@ -0,0 +1,9 @@
+<?php 
+$lang = $_GET["lang"] ?? "en";
+global $language;
+require_once($_SERVER['DOCUMENT_ROOT']."/view/Language/lang.".$lang.".php");
+
+include($_SERVER['DOCUMENT_ROOT']."/header.php");
+include($_SERVER['DOCUMENT_ROOT']."/".$lang."/projects/projects.html");
+include($_SERVER['DOCUMENT_ROOT']."/footer.php");
+?>
