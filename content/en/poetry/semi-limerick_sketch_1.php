3469db6c53a3111f3367b376ba507b070c9e775b
Bryan Kaperick <bryankaperick@gmail.com>
2024-10-15 13:11:54 +0200

    Fix title

--- content/en/poetry/semi-limerick_sketch_1.php
+++ content/en/poetry/semi-limerick_sketch_1.php
@@ -1,5 +1,5 @@
 <?php 
 include($_SERVER['DOCUMENT_ROOT']."/poems/minimal_poem_header.php");
 include($_SERVER['DOCUMENT_ROOT']."/en/minimal_header.html");
-include($_SERVER['DOCUMENT_ROOT']."/poems/./blocks/semi-limerick_sketch_#1.html");
+include($_SERVER['DOCUMENT_ROOT']."/poems/./blocks/semi-limerick_sketch_1.html");
 ?> 
--- content/fr/poetry/semi-limerick_sketch_1.php
+++ content/fr/poetry/semi-limerick_sketch_1.php
@@ -1,5 +1,5 @@
 <?php 
 include($_SERVER['DOCUMENT_ROOT']."/poems/minimal_poem_header.php");
 include($_SERVER['DOCUMENT_ROOT']."/fr/minimal_header.html");
-include($_SERVER['DOCUMENT_ROOT']."/poems/./blocks/semi-limerick_sketch_#1.html");
+include($_SERVER['DOCUMENT_ROOT']."/poems/./blocks/semi-limerick_sketch_1.html");
 ?> 
