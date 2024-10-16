8753b91719c905f17d3b8adc04eb5f2b1158816a
Bryan Kaperick <bryankaperick@gmail.com>
2024-10-16 17:54:01 +0200

    Nitpicks of icon

--- content/contact.php
+++ content/contact.php
@@ -3,9 +3,6 @@ $lang = $_GET["lang"] ?? "en";
 global $language;
 require_once($_SERVER['DOCUMENT_ROOT']."/view/Language/lang.".$lang.".php");
 
-$string = file_get_contents($_SERVER['DOCUMENT_ROOT']."/attributions/attributions.json");
-$p = json_decode($string);
-
 include($_SERVER['DOCUMENT_ROOT']."/header.php");
 include($_SERVER['DOCUMENT_ROOT']."/".$lang."/contact/contact.html");
 include($_SERVER['DOCUMENT_ROOT']."/footer.php");
