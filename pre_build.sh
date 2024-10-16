51972d868781df537f7251ee06b03bc39ee215e9
Bryan Kaperick <bryankaperick@gmail.com>
2024-10-16 16:45:24 +0200

    Prebuild

--- pre_build.sh
+++ pre_build.sh
@@ -3,18 +3,26 @@ echo "starting poetry ingestion"
 python3 ./scripts/instantiate.py
 python3 ./scripts/format_poem.py
 python3 ./scripts/create_html.py
+
 cd ../now
 echo "starting now ingestion"
 python3 instantiate.py "$1"
 ../../compress.sh
 python3 create_html.py > now.html
+
 cd ../photos
 echo "starting photo ingestion"
 python3 instantiate.py "$1"
 ../../compress.sh
 python3 create_html.py > photos.html
+
 cd ../widgets
 echo "starting widget ingestion"
 python3 create_html.py
+
+cd ../attributions
+echo "starting attributions ingestion"
+python3 create_html.py
+
 cd ../..
 echo "finished data ingestion"
