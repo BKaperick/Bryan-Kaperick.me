26532cdcda5578617b3863b44941a422cbfea4e5
Bryan Kaperick <bryankaperick@gmail.com>
2024-12-05 00:17:25 +0100

    Remote late night food stuff.

--- filter_audio.sh
+++ filter_audio.sh
@@ -1,6 +1,9 @@
-for f in $( find ../raw_audios); do
+for f in $( find ../raw_audios -type f); do
     newname=`echo "$f" | sed 's/raw_audios/bryankaperickme\/content\/poems\/recordings/'`
-    ffmpeg -i $f -af "highpass=f=200, lowpass=f=3000" $newname 
-    echo "current file is $f -> $newname"
+
+    if ! test -f $newname; then
+        ffmpeg -i $f -af "highpass=f=200, lowpass=f=3000" $newname 
+        echo "current file is $f -> $newname"
+    fi
     #mv "$f" "$newname"
 done
