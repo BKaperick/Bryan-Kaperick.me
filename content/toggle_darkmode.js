d8388abc0505dbbeee03af586b9560a8c85d396b
Bryan Kaperick <bryankaperick@gmail.com>
2025-01-08 18:23:32 +0100

    Also use prefers-color-scheme to auto-toggle darkmode

--- content/toggle_darkmode.js
+++ content/toggle_darkmode.js
@@ -1,6 +1,6 @@
 const root = document.documentElement;
 const toggle = document.getElementById("toggle");
-const darkMode = localStorage.getItem("dark-mode");
+const darkMode = localStorage.getItem("dark-mode") || (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
 if (darkMode) {
   root.classList.add("dark-theme");
 }
