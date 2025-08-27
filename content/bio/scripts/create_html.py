80fc36e6957fe39a4762e969408ecd60d74fb3bc
Bryan Kaperick <bryankaperick@gmail.com>
2025-08-27 15:37:52 +0200

    Convert books to table

--- content/bio/scripts/create_html.py
+++ content/bio/scripts/create_html.py
@@ -9,12 +9,33 @@ current_year = datetime.now().year
 
 def create_book_block(key, book):
     authors = ", ".join([a["name"] for a in book["authors"]])
-    return """<li><em><?=$p->{0}->title;?></em> &ndash; {1}</li>
-    """.format(key, authors)
+    month_ind = int(book["personal"]["finish_month"]) if "finish_month" in book["personal"] else 0
+    month = months_reverse[month_ind-1] + " " if month_ind > 0 else ""
+    year = int(book["personal"]["finish_year"])
+    return """
+    <tr>
+        <td><?=$p->{0}->title;?></td>
+        <td>{1}</td>
+        <td>{2}<?=$p->{0}->personal->finish_year;?></td>
+        <td><?=$p->{0}->personal->rating;?></td>
+    </tr>
+    """.format(key, authors, month)
+
 def date_getter(b):
     month = int(b["personal"]["finish_month"]) if "finish_month" in b["personal"] else 0
     year = 100 * int(b["personal"]["finish_year"])
     return year + month
+
+header = """
+<table class="bordered" border=1 frame=sides cellspacing="0" cellpadding="5">
+  <tr>
+    <th class="border1">Title</th>
+    <th class="border1">Author(s)</th>
+    <th class="border1">Finished</th>
+    <th class="border1">Rating (1-10)</th>
+  </tr>
+"""
+footer = """\n</table>"""
 
 with open("books.json", "r") as fr:
     books = json.load(fr)
@@ -27,6 +48,6 @@ with open("books.json", "r") as fr:
     for i,(key,book) in enumerate(ordered_books):
         block = create_book_block(key, book)
         blocks.append(block)
-    books_html = "<ul>\n" + "\n".join(blocks) + "\n</ul>"
+    books_html = header + "\n".join(blocks) + footer
     with open("books.html", "w") as p:
         p.write(books_html)
