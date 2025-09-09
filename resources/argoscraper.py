df150107579651db969091eff5c4b1e79a074ac7
Bryan Kaperick <bryankaperick@gmail.com>
2025-09-09 11:49:51 +0200

    Fix argo scraper - new headers, fix css, update to scrapy 2.13.

--- resources/argoscraper.py
+++ resources/argoscraper.py
@@ -2,6 +2,21 @@ from pathlib import Path
 from datetime import datetime
 import scrapy
 
+headers = {        
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',        
+    'Accept-Language': 'en-US,en;q=0.5',        
+    'Connection': 'keep-alive',        
+    'Cookie': 'AMCV_0D15148954E6C5100A4C98BC%40AdobeOrg=1176715910%7CMCIDTS%7C19271%7CMCMID%7C80534695734291136713728777212980602826%7CMCAAMLH-1665548058%7C7%7CMCAAMB-1665548058%7C6G1ynYcLPuiQxYZrsz_pkqfLG9yMXBpb2zX5dvJdYQJzPXImdj0y%7CMCOPTOUT-1664950458s%7CNONE%7CMCAID%7CNONE%7CMCSYNCSOP%7C411-19272%7CvVersion%7C5.4.0; s_ecid=MCMID%7C80534695734291136713728777212980602826; __cfruid=37ff2049fc4dcffaab8d008026b166001c67dd49-1664418998; AMCVS_0D15148954E6C5100A4C98BC%40AdobeOrg=1; s_cc=true; __cf_bm=NIDFoL5PTkinis50ohQiCs4q7U4SZJ8oTaTW4kHT0SE-1664943258-0-AVwtneMLLP997IAVfltTqK949EmY349o8RJT7pYSp/oF9lChUSNLohrDRIHsiEB5TwTZ9QL7e9nAH+2vmXzhTtE=; PHPSESSID=ddf49facfda7bcb4656eea122199ea0d',                        
+    'If-Modified-Since': 'Tue, 04 May 2021 05:09:49 GMT',        
+    'If-None-Match': 'W/"12c6a-5c17a16600f6c-gzip"',        
+    'Sec-Fetch-Dest': 'document',        
+    'Sec-Fetch-Mode': 'navigate',        
+    'Sec-Fetch-Site': 'none',        
+    'Sec-Fetch-User': '?1',        
+    'TE': 'trailers',        
+    'Upgrade-Insecure-Requests': '1',        
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0'        
+}
 
 '''
 Run with `$scrapy runspider argoscraper.py`
@@ -10,18 +25,18 @@ class ArgoSpider(scrapy.Spider):
     name = "argo"
     date_str = datetime.today().strftime('%Y%m%d_%H%M%S')
 
-    def start_requests(self):
+    async def start(self):
 
         urls = [
             "https://www.languefrancaise.net/Bob/Random",
         ]
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, headers=headers, callback=self.parse)
 
     def parse(self, response):
-        base_data = response.css("body")[0].css("div.container-fluid")[0].css("div.row")[0].css("div")[3].css("div.fpltemplate")[0]
+        base_data = response.css("body")[0].css("main#main")[0].css("div#wikitext")[0].css("div.fpltemplate")[0]
         # Definitions:
-        terms = base_data.css("a.wikilink::text").getall()
+        terms = base_data.css("a::text").getall()
         # Words:
         definitions = base_data.css("em::text").getall()
         page = response.url.split("/")[-3]
@@ -31,5 +46,4 @@ class ArgoSpider(scrapy.Spider):
                 clean_term = term.replace("?","").replace("■ ","").strip()
                 clean_defn = defn.replace("?","").replace("■ ","").strip()
                 f.write(f"*{clean_term}* -- {clean_defn}\n")
-
         self.log(f"Saved file {filename}")
--- resources/write_scraped_data.py
+++ resources/write_scraped_data.py
@@ -4,7 +4,7 @@ import sys
 from argoscraper import ArgoSpider
 from scrapy.crawler import CrawlerProcess
 content_path = sys.argv[1]
-file_regex = re.compile('www\.languefrancaise\.net-.*\.txt')
+file_regex = re.compile(r'www\.languefrancaise\.net-.*\.txt')
 
 def pop_from_file(fname):
     lines = open(fname).readlines()
@@ -18,15 +18,17 @@ def pop_from_file(fname):
 
 if __name__ == '__main__':
     job_done = False
-    while not job_done:
+    process = CrawlerProcess()
+    process.crawl(ArgoSpider)
+    process.start()
+    while True:
         for f in os.listdir():
             if file_regex.match(f):
+                print("----------------------- matching file - starting process (" + f + ")-------------------------------")
                 output = pop_from_file(f)
                 if output:
                     with open (content_path, 'w') as f:
                         f.write(output)
                         job_done = True
-        if not job_done:
-            process = CrawlerProcess()
-            process.crawl(ArgoSpider)
-            process.start()
+        if job_done:
+            break
