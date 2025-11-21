d904d0ef093d3f0f4503cb751febe8d641aa351f
Bryan Kaperick <bryankaperick@gmail.com>
2025-11-21 19:04:55 +0100

    restore changes to pre_build

--- pre_build.sh
+++ pre_build.sh
@@ -1,47 +1,43 @@
-#!/bin/bash
-
-echo $PATH
-
 cd ./content/poems
 echo "starting poetry ingestion"
-/__t/Python/3.13.9/x64/bin/python ./scripts/instantiate.py
-python ./scripts/format_poem.py all-poems
-python ./scripts/create_html.py
+python3 ./scripts/instantiate.py
+python3 ./scripts/format_poem.py all-poems
+python3 ./scripts/create_html.py
 
 cd ../now
 echo "starting now ingestion"
-python instantiate.py "$1"
+python3 instantiate.py "$1"
 ../../compress.sh
-python create_html.py > now.html
+python3 create_html.py > now.html
 
 cd ../photos
 echo "starting photo ingestion"
-python instantiate.py "$1"
+python3 instantiate.py "$1"
 ../../compress.sh
-python create_html.py > photos.html
+python3 create_html.py > photos.html
 
 cd ../widgets
 echo "starting widget ingestion"
 ./update_last_update.sh
-python create_html.py
+python3 create_html.py
 
 cd ../attributions
 echo "starting attributions ingestion"
-python create_html.py
+python3 create_html.py
 
 cd ../projects
 echo "starting project ingestion"
-python ./scripts/create_html.py
+python3 ./scripts/create_html.py
 
 cd ../bio
 echo "starting bio page ingestion"
-python ./scripts/create_html.py
-python ./scripts/ingest_films.py films.json
-python ./scripts/create_movie_html.py
+python3 ./scripts/create_html.py
+python3 ./scripts/ingest_films.py films.json
+python3 ./scripts/create_movie_html.py
 
 cd ../scripts
 echo "starting rss feed content creation"
-python ./create_rss.py
+python3 ./create_rss.py
 
 cd ../..
 echo "finished data ingestion"
