ed20391748f91b422ed33c94c67e15fa81e3f783
Bryan Kaperick <bryankaperick@gmail.com>
2026-04-17 17:20:22 +0200

    Fix index and copy over js files to public dir

--- cleanup.sh
+++ cleanup.sh
@@ -44,3 +44,4 @@ find . -type f -wholename "*" -exec sed -i -E "s/.php.html/.html/g" {} \;
 
 # HACK TODO fix blog post urls, `header.php` creates language toggle but doesnt have access to current path
 find . -type f -wholename "*/blog-post/*" -exec sed -i -E "s/\/(\w\w)\/(.*)\/(.*)\.html/\/\1\/blog-post\/\3.html/g" {} \;
+cp -r '../content/js' ./js
