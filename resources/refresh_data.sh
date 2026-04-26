8b9974453d457444b1227f7b44609aed2b68f98b
Bryan Kaperick <bryankaperick@gmail.com>
2026-04-26 12:04:57 +0200

    Make refresh_data agnostic to the run loc'

--- resources/refresh_data.sh
+++ resources/refresh_data.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-source ~/website-env/bin/activate
+source $DIR/../.venv/bin/activate
 python $DIR/../content/bio/scripts/ingest_films.py $DIR/../content/bio/films.json
 # python $DIR/datagetter.py $DIR/../content/widgets/datapoint.txt
 python $DIR/write_scraped_data.py $DIR/../content/widgets/argot.json
 echo "$DIR"
-git -C /home/bryan/Bryan-Kaperick.me pull origin --ff-only
-git -C /home/bryan/Bryan-Kaperick.me add "$DIR/../content/widgets/datapoint.txt"
-git -C /home/bryan/Bryan-Kaperick.me add "www*"
-git -C /home/bryan/Bryan-Kaperick.me add "$DIR/../content/widgets/argot.json"
-git -C /home/bryan/Bryan-Kaperick.me commit -m "Update data point"
-git -C /home/bryan/Bryan-Kaperick.me add "$DIR/../content/bio/films.json"
-git -C /home/bryan/Bryan-Kaperick.me diff-index --quiet HEAD || git -C /home/bryan/Bryan-Kaperick.me commit -m "Update films"
-git -C /home/bryan/Bryan-Kaperick.me push
+git -C $DIR/.. pull origin --ff-only
+git -C $DIR/.. add "$DIR/../content/widgets/datapoint.txt"
+git -C $DIR/.. add "www*"
+git -C $DIR/.. add "$DIR/../content/widgets/argot.json"
+git -C $DIR/.. commit -m "Update data point"
+git -C $DIR/.. add "$DIR/../content/bio/films.json"
+git -C $DIR/.. diff-index --quiet HEAD || git -C $DIR/.. commit -m "Update films"
+git -C $DIR/.. push
