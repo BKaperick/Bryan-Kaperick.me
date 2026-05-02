e102887b8f8cdc0bfaa1ce7a9ae1c6dfa82f370d
Bryan Kaperick <bryankaperick@gmail.com>
2026-05-02 18:40:42 +0200

    WIP auto populate poem counts by year

--- content/js/search.js
+++ content/js/search.js
@@ -1,20 +1,37 @@
 
-function populate_result_suggestions(results) {
+function run_search_query(index) {
+  const user_query = document.getElementById('search_query').value;
+  return index.search(user_query, { suggest: false });
+}
+
+function populate_result_suggestions(titles, results) {
   
   var suggestions = document.getElementById("partial_search_results");
   // Erase previous suggestions
   suggestions.innerHTML = ''
+  console.log("Query returned " + results.length + " results:")
   results.forEach((i,result_ind) => {
-    console.log(result_ind);
     var poem_block = document.getElementById(i);
-    console.log("Found: " + i);
+    console.log("Found: " + titles[i]);
     var option = document.createElement("option")
-    option.value = i
-    option.innerHTML = i;
+    option.value = titles[i]; // to make auto-complete
+    //option.innerHTML = titles[i]; // for display
                 
     suggestions.appendChild(option);
   })
 };
+
+function hide_nonmatching_results(keys, results) {
+  keys.forEach(key => {
+    var poem_block = document.getElementById(key);
+    if (results.indexOf(key) == -1) {
+      poem_block.style.display = 'none';
+    } else {
+      poem_block.style.display = 'block';
+    }
+
+  })
+}
 
 function redirect_from_results(results) {
   const user_query = document.getElementById('search_query').value;
@@ -26,7 +43,7 @@ function redirect_from_results(results) {
   results.forEach((i,result_ind) => {
     console.log(result_ind);
     var poem_block = document.getElementById(i);
-    console.log("Found: " + i);
+    // console.log("Found: " + i);
 
   if (result_ind == 0) {
     
