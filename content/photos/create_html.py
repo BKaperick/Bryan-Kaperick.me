ae979b9e3c10292bc8b449ed3bb1a1bf5649af1f
Bryan Kaperick <bryankaperick@gmail.com>
2026-05-16 13:52:37 +0200

    Clean up main func'

--- content/photos/create_html.py
+++ content/photos/create_html.py
@@ -2,16 +2,15 @@ import json
 import os
 import sys
 from math import sqrt, floor
-from typing import Any
 
 sys.path.append(os.path.abspath("../../"))
-from helper import order_photos
 from create_leaderboard import get_leaderboard, get_leaderboard_winner
+from helper import order_photos
 
 photo_blocks = []
 
 
-def get_grid(photo_count) -> str:
+def get_empty_grid(photo_count: int) -> str:
     """Define the grid for displaying an album on the main photos page."""
     rows = floor(sqrt(photo_count))
     columns = photo_count // rows
@@ -28,9 +27,12 @@ def get_grid(photo_count) -> str:
     return elem
 
 
+def get_filled_grid(photos: list[str]) -> str:
+    return get_empty_grid(len(photos)).format(*photos)
+
+
 def get_album_block(album, photo_blocks) -> str:
-    images = [x[0] for x in photo_blocks]
-    images_elem = get_grid(len(images)).format(*images)
+    images_elem = get_filled_grid(photo_blocks)
     caption = f"<?=$p->{album}->$lang;?> ~ <?=$p->{album}->year;?>"
 
     figure = get_figure("album", album, images_elem, caption)
@@ -97,6 +99,24 @@ def get_photo_captioned_figure_with_previous_next(
     full_caption = f"{prev_link}\n{caption}\n{next_link}"
 
     return get_figure("single-image", None, image, full_caption)
+
+
+def get_dedicated_page_photo_in_album(
+    key, has_caption: bool, album_key: str, photo_prev, photo_next
+):
+    prev_file_name = (
+        photo_prev["name"].replace(".jpg", "") if photo_prev is not None else None
+    )
+    next_file_name = (
+        photo_next["name"].replace(".jpg", "") if photo_next is not None else None
+    )
+    return get_photo_captioned_figure_with_previous_next(
+        key,
+        has_caption,
+        album_key,
+        prev_file_name,
+        next_file_name,
+    )
 
 
 def get_main_page_photo_block(key, file_name):
@@ -171,27 +191,31 @@ def generate_leaderboard():
     return "\n".join(data)
 
 
-def get_main_page_album_block(
-    key: str,
+def fill_album_prev_next_links(
+    key,
     photo,
     photo_key_to_album_key: dict[str, str],
     photo_key_to_previous: dict[str, str | None],
     photo_key_to_next: dict[str, str | None],
-) -> str:
-    album_blocks: list[tuple[Any, str]] = []
+):
     for i, subkey in enumerate(photo["photos"]):
         photo_key_to_album_key[subkey] = key
-
         # Used to assign the 'Previous' and 'Next links on the single-photo page when the photo is part of an album
         photo_key_to_previous[subkey] = None if i == 0 else photo["photos"][i - 1]
         photo_key_to_next[subkey] = (
             None if i == len(photo["photos"]) - 1 else photo["photos"][i + 1]
         )
 
-        sub_photo = photos[subkey]
-        sub_file_name = sub_photo["name"].replace(".jpg", "")
+
+def get_main_page_album_block(
+    key: str,
+    photo,
+) -> str:
+    album_blocks: list[str] = []
+    for i, subkey in enumerate(photo["photos"]):
+        sub_file_name = photos[subkey]["name"].replace(".jpg", "")
         sub_block = get_photo_block_in_album(subkey, sub_file_name)
-        album_blocks.append((sub_block, sub_photo))
+        album_blocks.append(sub_block)
     return get_album_block(key, album_blocks)
 
 
@@ -209,60 +233,52 @@ with open("photos.json", "r") as fw:
     photo_key_to_next: dict[str, str | None] = {}
 
     for key, photo in photos.items():
-        file_name = photo["name"].replace(".jpg", "")
-
-        # The element is an album
         if photo.get("is_album"):
-            block = get_main_page_album_block(
+            fill_album_prev_next_links(
                 key,
                 photo,
                 photo_key_to_album_key,
                 photo_key_to_previous,
                 photo_key_to_next,
             )
-            photo_blocks.append((block, photo))
-
-        # Photo is not contained in any album, so just print it on main photos page
-        elif key not in album_photos:
-            block = get_main_page_photo_block(key, file_name)
-            photo_blocks.append((block, photo))
-
-    # This gets piped to `./content/photos/photos.generated.html`
-    print(generate_leaderboard() + "\n\n")
-    print("\n\n".join([x[0] for x in sorted(photo_blocks, key=order_photos)]))
 
     # Get single-image pages (stored in raw_with_label)
     for key, photo in photos.items():
         file_name = photo["name"].replace(".jpg", "")
 
+        # MAIN PAGE
+
+        # The element is an album
+        if photo.get("is_album"):
+            main_block = get_main_page_album_block(
+                key,
+                photo,
+            )
+            photo_blocks.append((main_block, photo))
+
+        # Photo is not contained in any album, so just print it on main photos page
+        elif key not in album_photos:
+            main_block = get_main_page_photo_block(key, file_name)
+            photo_blocks.append((main_block, photo))
+
+        # DEDICATED PAGES
+
         # The idea is that if the photo is (1) contained in an album and (2) doesn't have its own description,
         # then on the single-image page, we use the album caption rather than leaving it at "~ [year]"
         if key in photo_key_to_album_key:
-            key_prev = photo_key_to_previous[key]
-            key_next = photo_key_to_next[key]
-            prev_file_name = (
-                photos[key_prev]["name"].replace(".jpg", "")
-                if key_prev is not None
-                else None
-            )
-            next_file_name = (
-                photos[key_next]["name"].replace(".jpg", "")
-                if key_next is not None
-                else None
-            )
-            block = get_photo_captioned_figure_with_previous_next(
+            ded_block = get_dedicated_page_photo_in_album(
                 key,
                 photo["en"] or photo["fr"],
                 photo_key_to_album_key[key],
-                prev_file_name,
-                next_file_name,
+                photos.get(photo_key_to_previous[key]),
+                photos.get(photo_key_to_next[key]),
             )
         else:
-            block = get_photo_captioned_figure(key, "raw")
+            ded_block = get_photo_captioned_figure(key, "raw")
 
         html_path = "./raw_with_label/" + file_name + ".generated.html"
         with open(html_path, "w") as f:
-            f.write(block)
+            f.write(ded_block)
         for language in ["en", "fr"]:
             php_path = "../{0}/photos/{1}.generated.php".format(language, file_name)
             with open(php_path, "w") as f:
@@ -274,3 +290,9 @@ include($_SERVER['DOCUMENT_ROOT']."/photos/{html_path}");
 ?> 
 """
                 )
+
+    with open("photos.generated.html", "w") as f:
+        f.write(generate_leaderboard())
+        f.write("\n\n\n")
+        f.write("\n\n".join([x[0] for x in sorted(photo_blocks, key=order_photos)]))
+        f.write("\n")
