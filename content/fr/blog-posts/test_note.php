47f39ed9e64996568218e171657f658e3136f0f3
Bryan Kaperick <bryankaperick@gmail.com>
2026-05-29 15:39:57 +0200

    Iterating on folder structure

---
+++ content/fr/blog-posts/test_note.php
@@ -0,0 +1,5 @@
+<?php 
+include($_SERVER['DOCUMENT_ROOT']."/posts/minimal_post_header.php");
+include($_SERVER['DOCUMENT_ROOT']."/fr/minimal_header.html");
+include($_SERVER['DOCUMENT_ROOT']."/posts/./blocks/test_note.generated.html");
+?> 
